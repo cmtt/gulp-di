f51b865d94eba86244db83b05961d5e2a0c665fe
Matthias Thoemmes <git@gmx.org>
2016-07-29 09:20:25 +0200

    Using internal resolver, additional tests for readDirectory

--- specs/03-read-directory.js
+++ specs/03-read-directory.js
@@ -3,10 +3,34 @@
 describe('readDirectory', () => {
   const readDirectory = require('../lib/read-directory');
   const path = require('path');
+
   it('reads examples', () => {
     let list = readDirectory(path.join(__dirname, '..', 'contrib/examples'));
-    console.log(list);
+    assert.equal(typeof list, 'object');
+    let keys = Object.keys(list);
+    assert.deepEqual(keys, [
+      'deg-to-rad',
+      'pi',
+      'rad-to-deg',
+      'to-deg',
+      'to-rad'
+    ]);
   });
 
-  it('camelize');
+  it('camelize', () => {
+    let list = readDirectory(path.join(__dirname, '..', 'contrib/examples'), true);
+    assert.equal(typeof list, 'object');
+    let keys = Object.keys(list);
+    assert.deepEqual(keys, [
+      'deg-to-rad',
+      'pi',
+      'rad-to-deg',
+      'to-deg',
+      'to-rad',
+      'degToRad',
+      'radToDeg',
+      'toDeg',
+      'toRad'
+    ]);
+  });
 });
